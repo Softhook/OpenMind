b48210975d1acc0c6b100bfef6c9111cf4377a6f
Christian Nold <christian@softhook.com>
2026-01-08 15:34:56 +0000

    Revert "version"
    This reverts commit ca2d5e49e5f4951cb9b9a782a33cb1b9967b89f6.

--- version.js
+++ version.js
@@ -7,33 +7,33 @@
 const APP_VERSION = {
     MAJOR: 1,
     MINOR: 0,
-    PATCH: 1,
-    
+    PATCH: 0,
+
     toString() {
         return `${this.MAJOR}.${this.MINOR}.${this.PATCH}`;
     },
-    
+
     toProtocolVersion() {
         return `${this.MAJOR}.${this.MINOR}`;
     },
-    
+
     checkCompatibility(other) {
         if (!other || typeof other.MAJOR !== 'number') {
             // Unknown version = incompatible (client is too old to have version support)
             return { compatible: false, shouldWarn: true, reason: 'Peer is running an outdated version without version support' };
         }
-        
+
         if (other.MAJOR !== this.MAJOR) {
             const isOlder = other.MAJOR < this.MAJOR;
             return {
                 compatible: false,
                 shouldWarn: true,
-                reason: isOlder 
+                reason: isOlder
                     ? 'Peer has older version - they need to refresh'
                     : 'You need to refresh - a newer version is available'
             };
         }
-        
+
         if (other.MINOR > this.MINOR) {
             return {
                 compatible: true,
@@ -41,7 +41,7 @@ const APP_VERSION = {
                 reason: 'A newer version is available - consider refreshing'
             };
         }
-        
+
         return { compatible: true, shouldWarn: false, reason: '' };
     }
 };
